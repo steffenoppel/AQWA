3be706afa4b1bc5f65e34547d24998b80d46c12b
Steffen Oppel <steffen.oppel@gmail.com>
2024-12-04 10:10:11 +0100

    explored various survival model formulations
    age variation in survival and sex variation in detection is by far the best model

--- AQWA_survival.R
+++ AQWA_survival.R
@@ -3,6 +3,8 @@
 ###################################################################################################
 # to be added to the IPM once we get this to run
 
+## updated on 4 Dec 2024 to explore various model formulations
+
 library(tidyverse)
 library(data.table)
 library(jagsUI)
@@ -61,24 +63,28 @@ jags.data <- list(nind=dim(AW_CH)[1],
 ### tried 3 model formulations: constant, sex.det, and age.sex, and DIC is lowest for age.sex
 
 
-sink("models/AQWA.surv.age.sex.jags")
+sink("models/AQWA.surv.age.sex.det.jags")
 cat("
     model {
       # Priors and constraints
       for (i in 1:nind){
         for (t in f[i]:(n.occasions-1)){
-          phi[i,t] <- beta[age[i,t], sex[i]]
-          #p[i,t] <- mean.p
+          phi[i,t] <- beta[age[i,t]]
+          p[i,t] <- mean.p[sex[i]]
         } #t
+        p[i,n.occasions] <- mean.p[sex[i]]
       } #i
       
       for (ag in 1:2){
-        for(sx in 1:2) {
-          beta[ag,sx] ~ dunif(0.2, 0.6)              # Priors for age- and sex-specific survival
-        }
+        beta[ag] ~ dunif(0.2, 0.6)              # Priors for age- and sex-specific survival
+        # for(sx in 1:2) {
+        #   beta[ag,sx] ~ dunif(0.2, 0.6)              # Priors for age- and sex-specific survival
+        # }
       }
+        for(sx in 1:2) {
+          mean.p[sx] ~ dunif(0.05, 0.75)                  # resighting probability is the same for all birds
+        }
       
-      mean.p ~ dunif(0.05, 0.75)                  # resighting probability is the same for all birds
       
       # Likelihood
   for (i in 1:nind){
@@ -88,7 +94,7 @@ cat("
       # State process
       z[i,t] ~ dbern(z[i,t-1] * phi[i,t-1])
       # Observation process
-      y[i,t] ~ dbern(z[i,t] * mean.p)
+      y[i,t] ~ dbern(z[i,t] * p[i,t])
     } #t
   } #i
     }
@@ -99,10 +105,10 @@ sink()
 
 inits <- function(){list(
   z= zInit(as.matrix(AW_CH[,3:7])),
-  beta= matrix(runif(4, 0.4,0.59),ncol=2),
-  #beta= runif(2, 0.4,0.59),
+  #beta= matrix(runif(4, 0.4,0.59),ncol=2),
+  beta= runif(2, 0.4,0.59),
   #mean.phi= runif(1, 0.4,0.59),
-  mean.p = runif(1, 0.2,0.7))}
+  mean.p = runif(2, 0.2,0.7))}
 
 # Parameters monitored
 parameters <- c("beta","mean.p")
@@ -119,7 +125,7 @@ nc <- 4
 surv.model <- jags(jags.data,
                   inits,
                   parameters,
-                  "models/AQWA.surv.age.sex.jags",
+                  "models/AQWA.surv.age.sex.det.jags",
                   n.chains = nc, n.thin = nt, n.iter = ni, n.burnin = nb, n.cores=nc, parallel=T)
 
 
