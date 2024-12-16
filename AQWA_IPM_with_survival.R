bf755f8dafbcc2ce7dd02a75a52cc5eb3df96748
Steffen Oppel <steffen.oppel@gmail.com>
2024-12-16 19:09:09 +0100

    added new population projections with more scenarios

--- AQWA_IPM_with_survival.R
+++ AQWA_IPM_with_survival.R
@@ -530,21 +530,53 @@ ntotdf$cip <- as.numeric(t(ipm.model$q97.5$Ntot))
 
 ntotdf$Scenario <- factor(ntotdf$Scenario, levels = c("Mowing + 5y Habitat Unconstrained", "Mowing + 10y Habitat Unconstrained", "Mowing + 20y Habitat Unconstrained",
                                                       "No mowing + 5y Habitat Unconstrained", "No mowing + 10y Habitat Unconstrained","No mowing + 20y Habitat Unconstrained",
-                                                      "No mowing + 5y Habitat Constrained 120 ha", "No mowing + 10y Habitat Constrained 120 ha","No mowing + 20y Habitat Constrained 120 ha",
-                                                      "No mowing + 5y Habitat Constrained 240 ha", "No mowing + 10y Habitat Constrained 240 ha","No mowing + 20y Habitat Constrained 240 ha",
-                                                      "No mowing + surv improve 5% + 5y Habitat 120 ha", "No mowing + surv improve 5% + 10y Habitat 120 ha","No mowing + surv improve 5% + 20y Habitat 120 ha"))
+                                                      "No mowing + 5y Habitat Constrained 200 ha", "No mowing + 10y Habitat Constrained 200 ha","No mowing + 20y Habitat Constrained 200 ha",
+                                                      "No mowing + 5y Habitat Constrained 400 ha", "No mowing + 10y Habitat Constrained 400 ha","No mowing + 20y Habitat Constrained 400 ha",
+                                                      "No mowing + 5y Habitat Constrained 1200 ha", "No mowing + 10y Habitat Constrained 1200 ha","No mowing + 20y Habitat Constrained 1200 ha",
+                                                      "No mowing + 5y Habitat Constrained 2400 ha", "No mowing + 10y Habitat Constrained 2400 ha","No mowing + 20y Habitat Constrained 2400 ha",
+                                                      "Past trajectory without mowing"))
 
-ntotdf$year <- factor(ntotdf$year)
+#ntotdf$year <- factor(ntotdf$year)
 
-#The plot
+#The plot for the past (with hypothetical changes of not mowing and not mowing and survival improvement)
 
-poptrends <- ggplot(data = ntotdf, aes(x = year, y = Ntot, col = Scenario, group = Scenario)) +
-  geom_line(linewidth = 1.1) + geom_vline(aes(xintercept = 23), linetype = 2) +
+poptrendspast <- 
+  ntotdf %>%
+  filter(Scenario=="Past trajectory without mowing") %>%
+  filter(year>21) %>%
+  mutate(year=year-21) %>%
+  bind_rows(ntotdf %>%
+              filter(Scenario=="Mowing + 5y Habitat Unconstrained") %>%
+              filter(year<22) %>%
+              mutate(Scenario="Observed past")
+  ) %>%
+  
+  ggplot(aes(x = year+2002, y = Ntot, col = Scenario, group = Scenario)) +
+  geom_line(linewidth = 1.1) +
   geom_ribbon(aes(ymin = cim, ymax = cip), linetype = 2, fill = NA) +
-  facet_wrap(~Scenario, nrow = 5, scales="free_y")  +
+  facet_grid(survival~Scenario, scales="free_y")  +
   theme_bw() +
   theme(legend.position = "none") +
-  scale_x_discrete(breaks = seq(from = 0, to = 42, by = 6)) + xlab("") + ylab("")
+  xlab("Year") + ylab("Aquatic Warbler population size")
+poptrendspast
+ggsave("output/Past_population_trajectories.jpg", width=241,height=191, quality=100, units="mm")
+
+
+
+#The plot for the future
+
+poptrends <- 
+  ntotdf %>%
+  filter(Scenario!="Past trajectory without mowing") %>%
+
+  ggplot(aes(x = year+2002, y = Ntot, col = Scenario, linetype=survival, group = survival)) +
+  geom_line(linewidth = 1.1) +
+  geom_vline(aes(xintercept = 2025), linetype = 2) +
+  geom_ribbon(aes(ymin = cim, ymax = cip,linetype=survival), fill = NA) +
+  facet_wrap(~Scenario, ncol = 3, scales="free_y")  +
+  theme_bw() +
+  theme(legend.position = "none") +
+  xlab("Year") + ylab("Aquatic Warbler population size")
 poptrends
 #ggsave("output/Scenario_projections.jpg", width=241,height=191, quality=100, units="mm")
 
@@ -577,50 +609,47 @@ fut.ext<-as_tibble(rbind(ipm.model$samples[[1]],ipm.model$samples[[2]],ipm.model
   dplyr::select(tidyselect::starts_with("Ntot")) %>%
   dplyr::select(tidyselect::ends_with(",42]"))
 
-names(fut.ext)<- c("Mowing + 05y Habitat Unconstrained",
-                   "Mowing + 10y Habitat Unconstrained", 
-                   "Mowing + 20y Habitat Unconstrained", 
-                   "No mowing + 05y Habitat Unconstrained", 
-                   "No mowing + 10y Habitat Unconstrained", 
-                   "No mowing + 20y Habitat Unconstrained", 
-                   "No mowing + 05y Habitat Constrained 120 ha", 
-                   "No mowing + 10y Habitat Constrained 120 ha", 
-                   "No mowing + 20y Habitat Constrained 120 ha", 
-                   "No mowing + 05y Habitat Constrained 240 ha", 
-                   "No mowing + 10y Habitat Constrained 240 ha", 
-                   "No mowing + 20y Habitat Constrained 240 ha", 
-                   "No mowing + surv improve 5% + 05y Habitat 120 ha", 
-                   "No mowing + surv improve 5% + 10y Habitat 120 ha", 
-                   "No mowing + surv improve 5% + 20y Habitat 120 ha")
+names(fut.ext)<- rep(c("Mowing + 5y Habitat Unconstrained", "Mowing + 10y Habitat Unconstrained", "Mowing + 20y Habitat Unconstrained",
+                   "No mowing + 5y Habitat Unconstrained", "No mowing + 10y Habitat Unconstrained","No mowing + 20y Habitat Unconstrained",
+                   "No mowing + 5y Habitat Constrained 200 ha", "No mowing + 10y Habitat Constrained 200 ha","No mowing + 20y Habitat Constrained 200 ha",
+                   "No mowing + 5y Habitat Constrained 400 ha", "No mowing + 10y Habitat Constrained 400 ha","No mowing + 20y Habitat Constrained 400 ha",
+                   "No mowing + 5y Habitat Constrained 1200 ha", "No mowing + 10y Habitat Constrained 1200 ha","No mowing + 20y Habitat Constrained 1200 ha",
+                   "No mowing + 5y Habitat Constrained 2400 ha", "No mowing + 10y Habitat Constrained 2400 ha","No mowing + 20y Habitat Constrained 2400 ha",
+                   "Past trajectory without mowing"), 2)
 
 
 fut.ext %>%
   gather(key="Scenario",value="N") %>%
+  mutate(survival=rep(c("no survival improvement","5% survival improvement"), each=(nscenarios/2)*40000)) %>%
   filter(!(str_detect(pattern="20y", string=Scenario))) %>%
-  group_by(Scenario) %>%
+  filter(Scenario!="Past trajectory without mowing") %>%
+  group_by(Scenario,survival) %>%
   summarize(ext.prob=w0(N)) %>%
   
   ### start the plot ###
   ggplot(aes(x = Scenario, y=ext.prob, fill = Scenario)) +                       # Draw overlaying histogram
   geom_bar(stat="identity",alpha = 0.6) +
+  facet_wrap(~survival, ncol = 1)  +
   labs(x="Future scenarios", y="Probability of extinction within 20 years",
        fill="Scenario") +
   scale_fill_discrete() +
   
   theme(panel.background=element_rect(fill="white", colour="black"), 
-        axis.text.y=element_text(size=18, color="black"),
+        axis.text.y=element_text(size=14, color="black"),
         axis.text.x=element_blank(), 
-        axis.title=element_text(size=20),
-        legend.text=element_text(size=12),
-        legend.title = element_text(size=14),
-        legend.position="inside",
-        legend.position.inside=c(0.65,0.80),
+        axis.title=element_text(size=16),
+        strip.text=element_text(size=14, color="black"),
+        strip.background=element_rect(fill="white", colour="black"),
+        legend.text=element_text(size=10),
+        legend.title = element_text(size=12),
+        legend.position="bottom",
+        legend.direction="horizontal",
         panel.grid.major = element_line(size=.1, color="grey94"),
         panel.grid.minor = element_blank(),
         panel.border = element_rect(fill=NA, colour = "black"))
 
 
-ggsave("output/Extinction_probability.jpg", width=181,height=141, quality=100, units="mm")
+ggsave("output/Extinction_probability.jpg", width=231,height=191, quality=100, units="mm")
 
 
 
@@ -644,13 +673,16 @@ ggsave("output/Extinction_probability.jpg", width=181,height=141, quality=100, u
 
 as_tibble(rbind(ipm.model$samples[[1]],ipm.model$samples[[2]],ipm.model$samples[[3]],ipm.model$samples[[4]])) %>%
   dplyr::select(tidyselect::starts_with("proj.lambda")) %>%
-  rename(nochange=`proj.lambda[1]`,no.mowing.unlim.hab=`proj.lambda[4]`,no.mowing.lim.hab=`proj.lambda[7]`,imp.surv=`proj.lambda[13]`) %>%
+  rename(nochange=`proj.lambda[1]`,
+         no.mowing.unlim.hab=`proj.lambda[4]`,
+         no.mowing.lim.hab.200=`proj.lambda[7]`,
+         no.mowing.lim.hab.1200=`proj.lambda[13]`) %>%
   dplyr::select(-tidyselect::starts_with("proj.lambda")) %>%
   gather(key="Scenario",value="lambda") %>%
   filter(lambda>0.5) %>% ## remove bizarre growth rates caused by extinct populations?
-  mutate(Scenario=if_else(Scenario=="imp.surv", "no mowing + higher survival",
+  mutate(Scenario=if_else(Scenario=="no.mowing.lim.hab.200", "no mowing - 200 ha habitat",
                           if_else(Scenario=="nochange", "with current mowing pressure",
-                          if_else(Scenario=="no.mowing.unlim.hab","no mowing - unlimited habitat","no mowing - 120 ha habitat")))) %>%
+                          if_else(Scenario=="no.mowing.unlim.hab","no mowing - unlimited habitat","no mowing - 1200 ha habitat")))) %>%
   
   ### start the plot ###
   ggplot(aes(x = lambda, fill = Scenario)) +                       # Draw overlaying histogram
@@ -681,82 +713,82 @@ ggsave("output/Future_pop_growth_rates.jpg", width=181,height=141, quality=100, 
 
 
 #On the retrospective analysis (past trends), see how inference has modified the priors and the estimates.
-
-par(mfrow = c(2,2))
-
-#Juvenile survival
-
-plot(ipm.model$mean$phij, type = "l", col = "green", ylim = c(0.18, 0.45), lwd = 2, xlab = "Year", ylab = "Estimate", main = "Juvenile survival")
-lines(ipm.model$q2.5$phij, lty = "dashed", lwd = 1.5, col = "green")
-lines(ipm.model$q97.5$phij, lty = "dashed", lwd = 1.5, col = "green")
-abline(v = ny, lty = "dashed")
-
-#Adult survival
-
-plot(ipm.model$mean$phia, type = "l", col = "green", ylim = c(0.25,0.6), lwd = 2, xlab = "Year", ylab = "Estimate", main = "Adult survival")
-lines(ipm.model$q2.5$phia, lty = "dashed", lwd = 1.5, col = "green")
-lines(ipm.model$q97.5$phia, lty = "dashed", lwd = 1.5, col = "green")
-abline(v = ny, lty = "dashed")
-
-#First-year productivity
-
-plot(ipm.model$mean$fec1, type = "l", col = "red", ylim = c(1.5,5.6), lwd = 2, xlab = "Year", ylab = "Estimate", main = "First-year productivity")
-lines(ipm.model$q2.5$fec1, lty = "dashed", lwd = 1.5, col = "red")
-lines(ipm.model$q97.5$fec1, lty = "dashed", lwd = 1.5, col = "red")
-abline(v = ny, lty = "dashed")
-
-#Second-year productivity
-
-plot(ipm.model$mean$fec2, type = "l", col = "red", ylim = c(0.7,3.2), lwd = 2, xlab = "Year", ylab = "Estimate", main = "Second-year productivity")
-lines(ipm.model$q2.5$fec2, lty = "dashed", lwd = 1.5, col = "red")
-lines(ipm.model$q97.5$fec2, lty = "dashed", lwd = 1.5, col = "red")
-abline(v = ny, lty = "dashed")
-
-#See how priors compare to final estimates
-
-par(mfrow = c(2,2))
-
-#Adult survival
-
-adprior <- dnorm(seq(from = 0, to = 1, by = 0.01), mean = 0.42, sd = 0.03)
-plot(adprior, type = "l", xaxt = "n", ylab = "", xlab = "Estimate value", main = "Adult Survival \n Red = Prior, Blue = Estimate", ylim = c(0,15), col = "red", lwd = 2)
-axis(1, at = seq(from = 0, to = 100, by = 10), labels = seq(from = 0, to = 1, by = 0.1))
-ipm.model$mean$mphi[2]
-ipm.model$sd$mphi[2]
-adresult <- dnorm(seq(from = 0, to = 1, by = 0.01), mean = ipm.model$mean$mphi[2], sd = ipm.model$sd$mphi[2])
-lines(adresult, type = "l", lty = "dashed", col = "blue", lwd = 2)
-
-#Juvenile survival
-
-juvprior <- dnorm(seq(from = 0, to = 1, by = 0.01), mean = 0.32, sd = 0.025)
-plot(juvprior, type = "l", xaxt = "n", ylab = "", xlab = "Estimate value", main = "Juvenile Survival \n Red = Prior, Blue = Estimate", ylim = c(0,19), col = "red", lwd = 2)
-axis(1, at = seq(from = 0, to = 100, by = 10), labels = seq(from = 0, to = 1, by = 0.1))
-ipm.model$mean$mphi[1]
-ipm.model$sd$mphi[1]
-juvresult <- dnorm(seq(from = 0, to = 1, by = 0.01), mean = ipm.model$mean$mphi[1], sd = ipm.model$sd$mphi[1])
-lines(juvresult, type = "l", lty = "dashed", col = "blue", lwd = 2)
-
-#First-brood productivity
-
-p1prior <- dnorm(seq(from = 2, to = 4, by = 0.1), mean = 3.2, sd = 0.16)
-plot(p1prior, type = "l", xaxt = "n", ylab = "", xlab = "Estimate value", main = "First-brood productivity \n Red = Prior, Blue = Estimate", ylim = c(0,3), col = "red", lwd = 2)
-axis(1, at = seq(from = 1, to = 21, by = 1), labels = seq(from = 2, to = 4, by = 0.1))
-ipm.model$mean$mfec1
-ipm.model$sd$mfec1
-p1result <- dnorm(seq(from = 2, to = 4, by = 0.1), mean = ipm.model$mean$mfec1, sd = ipm.model$sd$mfec1)
-lines(p1result, type = "l", lty = "dashed", col = "blue", lwd = 2)
-
-#Second-brood productivity
-
-p2prior <- dnorm(seq(from = 0.5, to = 3.2, by = 0.1), mean = 1.75, sd = 0.15)
-plot(p2prior, type = "l", xaxt = "n", ylab = "", xlab = "Estimate value", main = "Second-brood productivity \n Red = Prior, Blue = Estimate", ylim = c(0,3), col = "red", lwd = 2)
-axis(1, at = seq(from = 1, to = 28, by = 1), labels = seq(from = 0.5, to = 3.2, by = 0.1))
-ipm.model$mean$mfec2
-ipm.model$sd$mfec2
-p2result <- dnorm(seq(from = 0.5, to = 3.2, by = 0.1), mean = ipm.model$mean$mfec2, sd = ipm.model$sd$mfec2)
-lines(p2result, type = "l", lty = "dashed", col = "blue", lwd = 2)
-
-#Compared to models where we allowed second broods, here the change in the estimates is very very mild.
+## NOT UPDATED THIS CODE WITH SCENARIOS V4
+# par(mfrow = c(2,2))
+# 
+# #Juvenile survival
+# 
+# plot(ipm.model$mean$phij, type = "l", col = "green", ylim = c(0.18, 0.45), lwd = 2, xlab = "Year", ylab = "Estimate", main = "Juvenile survival")
+# lines(ipm.model$q2.5$phij, lty = "dashed", lwd = 1.5, col = "green")
+# lines(ipm.model$q97.5$phij, lty = "dashed", lwd = 1.5, col = "green")
+# abline(v = ny, lty = "dashed")
+# 
+# #Adult survival
+# 
+# plot(ipm.model$mean$phia, type = "l", col = "green", ylim = c(0.25,0.6), lwd = 2, xlab = "Year", ylab = "Estimate", main = "Adult survival")
+# lines(ipm.model$q2.5$phia, lty = "dashed", lwd = 1.5, col = "green")
+# lines(ipm.model$q97.5$phia, lty = "dashed", lwd = 1.5, col = "green")
+# abline(v = ny, lty = "dashed")
+# 
+# #First-year productivity
+# 
+# plot(ipm.model$mean$fec1, type = "l", col = "red", ylim = c(1.5,5.6), lwd = 2, xlab = "Year", ylab = "Estimate", main = "First-year productivity")
+# lines(ipm.model$q2.5$fec1, lty = "dashed", lwd = 1.5, col = "red")
+# lines(ipm.model$q97.5$fec1, lty = "dashed", lwd = 1.5, col = "red")
+# abline(v = ny, lty = "dashed")
+# 
+# #Second-year productivity
+# 
+# plot(ipm.model$mean$fec2, type = "l", col = "red", ylim = c(0.7,3.2), lwd = 2, xlab = "Year", ylab = "Estimate", main = "Second-year productivity")
+# lines(ipm.model$q2.5$fec2, lty = "dashed", lwd = 1.5, col = "red")
+# lines(ipm.model$q97.5$fec2, lty = "dashed", lwd = 1.5, col = "red")
+# abline(v = ny, lty = "dashed")
+# 
+# #See how priors compare to final estimates
+# 
+# par(mfrow = c(2,2))
+# 
+# #Adult survival
+# 
+# adprior <- dnorm(seq(from = 0, to = 1, by = 0.01), mean = 0.42, sd = 0.03)
+# plot(adprior, type = "l", xaxt = "n", ylab = "", xlab = "Estimate value", main = "Adult Survival \n Red = Prior, Blue = Estimate", ylim = c(0,15), col = "red", lwd = 2)
+# axis(1, at = seq(from = 0, to = 100, by = 10), labels = seq(from = 0, to = 1, by = 0.1))
+# ipm.model$mean$mphi[2]
+# ipm.model$sd$mphi[2]
+# adresult <- dnorm(seq(from = 0, to = 1, by = 0.01), mean = ipm.model$mean$mphi[2], sd = ipm.model$sd$mphi[2])
+# lines(adresult, type = "l", lty = "dashed", col = "blue", lwd = 2)
+# 
+# #Juvenile survival
+# 
+# juvprior <- dnorm(seq(from = 0, to = 1, by = 0.01), mean = 0.32, sd = 0.025)
+# plot(juvprior, type = "l", xaxt = "n", ylab = "", xlab = "Estimate value", main = "Juvenile Survival \n Red = Prior, Blue = Estimate", ylim = c(0,19), col = "red", lwd = 2)
+# axis(1, at = seq(from = 0, to = 100, by = 10), labels = seq(from = 0, to = 1, by = 0.1))
+# ipm.model$mean$mphi[1]
+# ipm.model$sd$mphi[1]
+# juvresult <- dnorm(seq(from = 0, to = 1, by = 0.01), mean = ipm.model$mean$mphi[1], sd = ipm.model$sd$mphi[1])
+# lines(juvresult, type = "l", lty = "dashed", col = "blue", lwd = 2)
+# 
+# #First-brood productivity
+# 
+# p1prior <- dnorm(seq(from = 2, to = 4, by = 0.1), mean = 3.2, sd = 0.16)
+# plot(p1prior, type = "l", xaxt = "n", ylab = "", xlab = "Estimate value", main = "First-brood productivity \n Red = Prior, Blue = Estimate", ylim = c(0,3), col = "red", lwd = 2)
+# axis(1, at = seq(from = 1, to = 21, by = 1), labels = seq(from = 2, to = 4, by = 0.1))
+# ipm.model$mean$mfec1
+# ipm.model$sd$mfec1
+# p1result <- dnorm(seq(from = 2, to = 4, by = 0.1), mean = ipm.model$mean$mfec1, sd = ipm.model$sd$mfec1)
+# lines(p1result, type = "l", lty = "dashed", col = "blue", lwd = 2)
+# 
+# #Second-brood productivity
+# 
+# p2prior <- dnorm(seq(from = 0.5, to = 3.2, by = 0.1), mean = 1.75, sd = 0.15)
+# plot(p2prior, type = "l", xaxt = "n", ylab = "", xlab = "Estimate value", main = "Second-brood productivity \n Red = Prior, Blue = Estimate", ylim = c(0,3), col = "red", lwd = 2)
+# axis(1, at = seq(from = 1, to = 28, by = 1), labels = seq(from = 0.5, to = 3.2, by = 0.1))
+# ipm.model$mean$mfec2
+# ipm.model$sd$mfec2
+# p2result <- dnorm(seq(from = 0.5, to = 3.2, by = 0.1), mean = ipm.model$mean$mfec2, sd = ipm.model$sd$mfec2)
+# lines(p2result, type = "l", lty = "dashed", col = "blue", lwd = 2)
+# 
+# #Compared to models where we allowed second broods, here the change in the estimates is very very mild.
 
 ############################################################################
 #
@@ -767,7 +799,7 @@ out<-as.data.frame(ipm.model$summary)
 out$parameter<-row.names(ipm.model$summary)
 names(out)[c(12,5,3,7)]<-c('parm','median','lcl','ucl')
 print(ipm.model, dig=3)
-write.table(out, "output/AQWA_GER_model_nscenarios_output.v2.csv", sep=",")
+write.table(out, "output/AQWA_GER_model_nscenarios_output.v4.csv", sep=",")
 
 
 
